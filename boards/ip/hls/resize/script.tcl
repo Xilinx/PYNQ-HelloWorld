0e69c5a40885b64397dfeda2cd468ffe868dce1d
Rock Qu <yunq@xilinx.com>
2019-02-28 15:04:38 -0800

    fix hls ip

--- boards/ip/hls/resize/script.tcl
+++ boards/ip/hls/resize/script.tcl
@@ -3,22 +3,20 @@
 ## Please DO NOT edit it.
 ## Copyright (C) 1986-2017 Xilinx, Inc. All Rights Reserved.
 ############################################################
-open_project hls_proj
+open_project resize
 set_top resize_accel
-add_files xf_resize_accel.cpp -cflags "-I../include -std=c++0x"
-add_files xf_resize_config.h
-add_files xf_config_params.h
+add_files resize/xf_resize_accel.cpp -cflags "-Iresize/include"
+add_files resize/xf_resize_config.h
+add_files resize/xf_config_params.h
 
-add_files -tb xf_headers.h
-add_files -tb xf_resize_tb.cpp -cflags "-I../include -std=c++0x"
-add_files -tb /proj/xsjhdstaff4/varuns/DATA/img_l.png
+add_files -tb resize/xf_headers.h
+add_files -tb resize/xf_resize_tb.cpp -cflags "-Iresize/include"
 
 open_solution "solution1"
 set_part {xc7z020clg484-1} -tool vivado
 create_clock -period 10 -name default
 set_clock_uncertainty 27.0%
 
-csim_design -argv {img_l.png} -clean -compiler gcc
 csynth_design
-cosim_design -argv {img_l.png}
-export_design -format ip_catalog
+export_design -format ip_catalog -description "Image resizer based on HLS" -display_name "Resizer"
+exit
