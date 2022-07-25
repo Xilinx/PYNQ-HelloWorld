842e5da64f4858ed4acc87e3e239a0c5bd8ab661
Mario Ruiz <mruiznog@amd.com>
2022-07-25 10:43:26 +0100

    Update ps version

--- boards/ZCU104/resizer/resizer.tcl
+++ boards/ZCU104/resizer/resizer.tcl
@@ -130,7 +130,7 @@ xilinx.com:ip:axi_dma:7.1\
 xilinx.com:ip:axis_dwidth_converter:1.1\
 xilinx.com:hls:resize_accel:1.0\
 xilinx.com:ip:proc_sys_reset:5.0\
-xilinx.com:ip:zynq_ultra_ps_e:3.3\
+xilinx.com:ip:zynq_ultra_ps_e:3.4\
 "
 
    set list_ips_missing ""
@@ -246,7 +246,7 @@ proc create_root_design { parentCell } {
  ] $rst_clk_wiz_100M
 
   # Create instance: zynq_ultra_ps_e_0, and set properties
-  set zynq_ultra_ps_e_0 [ create_bd_cell -type ip -vlnv xilinx.com:ip:zynq_ultra_ps_e:3.3 zynq_ultra_ps_e_0 ]
+  set zynq_ultra_ps_e_0 [ create_bd_cell -type ip -vlnv xilinx.com:ip:zynq_ultra_ps_e:3.4 zynq_ultra_ps_e_0 ]
   set_property -dict [ list \
    CONFIG.PSU_BANK_0_IO_STANDARD {LVCMOS18} \
    CONFIG.PSU_BANK_1_IO_STANDARD {LVCMOS18} \
